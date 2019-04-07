655f1c7aebefbb6c88dd374be76a680e5d014ad1
Mikimoto <mikimoto.c@gmail.com>
2019-04-07 22:37:54 +0800

    新增第一次翻譯

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *                     DeepThought Taiwan language package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the GPLv3 License
+ *                     that is bundled with this package in the file LICENSE.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://github.com/DeepThoughtTech/magento-2-chinese-taiwan-language-pack/blob/master/LICENSE
+ *
+ *                     @category  DeepThought
+ *                     @package   DeepThought_Magento2_Taiwan_Language
+ *                     @copyright Copyright (c) 2019
+ *                     @license   https://github.com/DeepThoughtTech/magento-2-chinese-taiwan-language-pack/blob/master/LICENSE
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+	'deepthought_zh_hans_tw',
+	__DIR__
+);
